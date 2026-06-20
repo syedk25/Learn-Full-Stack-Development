93f24abdbd9eecf1b345953a50bd710d11f8c151
syedk25 <k.syed0787@gmail.com>
2026-06-20 23:44:03 +0530

    save project

--- 4)React/React_Learn/6)Context-Api/src/components/Content.jsx
+++ 4)React/React_Learn/6)Context-Api/src/components/Content.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Shop from './Shop'
-
+import ProductDetails from './ProductDetails';
+import { useState } from 'react';
 
 function Content() {
+  const [products, setProducts] = useState({ id: 1, name: 'IPhone 17 pro', price: 100000, decscription: '8GB/256GB' });
   return (
     <main>
-      <Shop/>
+      <h1>Welcome to My Shop!</h1>
+      <ProductDetails products={products}
+      />
     </main>
   )
 }
--- 4)React/React_Learn/6)Context-Api/src/components/ProductDetails.jsx
+++ 4)React/React_Learn/6)Context-Api/src/components/ProductDetails.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../App'
 
-function ProductDetails({ name = 'Unspecified', price = 0, description = 'Not specified' }) {
+function ProductDetails({ products }) 
+{
+  let {uname,price,description}=products;
 
   let { user } = useContext(UserContext);
 
-  console.log('User from context:', user);
+  console.log('User from context:',user);
 
   return (
     <div>
       <section>
         <article>
-          <h1>UserName :  {user.uname}</h1>
+          <h1>UserName :  {uname}</h1>
         </article>
         <h2>{name}</h2>
         <p>Price: ${price.toFixed(2)}</p>
